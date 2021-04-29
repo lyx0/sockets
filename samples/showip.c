87035776f53b0c82ec3900383ebba816ade7b641
lyx0 <stefan.parfuss@protonmail.ch>
2021-04-29 04:29:09 +0200

    forgot a tab

--- samples/showip.c
+++ samples/showip.c
@@ -39,9 +39,9 @@ int main(int argc, char *argv[])
         // get the pointer to the address itself,
         // different fields in IPv4 and IPv6:
         if (p->ai_family == AF_INET) {  // IPv4 
-        struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-        addr = &(ipv4->sin_addr);
-        ipver = "IPv4";
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
         } else {  // IPv6
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
             addr = &(ipv6->sin6_addr);
